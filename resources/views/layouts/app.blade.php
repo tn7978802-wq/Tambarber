41c2356ffdfc6e74dcc11261f1e567c7c57e0de3
Nguyen Van Toan <tn7978802@gmail.com>
2026-08-22 21:47:10 +0700

    1

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>@yield('title', 'Tâm     Barbershop')</title>
+    <title>@yield('title', 'TâmBarbershop')</title>
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <link rel="icon" type="image/png" href="{{ asset('images/barber.png') }}">
 </head>
