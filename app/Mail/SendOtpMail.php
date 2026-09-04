a497a5aaffc346a22fbe0456456f9ebbaddc3b7a
Nguyen Van Toan <tn7978802@gmail.com>
2026-09-04 10:13:34 +0700

    commit

--- app/Mail/SendOtpMail.php
+++ app/Mail/SendOtpMail.php
@@ -4,11 +4,12 @@ namespace App\Mail;
 
 use Illuminate\Bus\Queueable;
 use Illuminate\Mail\Mailable;
+use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
 
-class SendOtpMail extends Mailable
+class SendOtpMail extends Mailable implements ShouldQueue
 {
     use Queueable, SerializesModels;
 
